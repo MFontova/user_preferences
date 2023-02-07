699adf2fb0b08ba049a3d128b87e7643c4734dfa
Marc Fontova <mfontovat@gmail.com>
2023-02-07 16:02:38 +0100

    195.1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeScreen'),
+      ),
       body: Center(
         child: Text('HomeScreen'),
       ),
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -6,7 +6,10 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
       body: Center(
         child: Text('SettingsScreen'),
       ),
